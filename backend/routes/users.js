517977aca06cc189f6bb255b386a3c5face0b601
Yasharth dubey <yashdubeywinner@gmail.com>
2021-01-31 21:29:41 +0530

    working on backend

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -3,21 +3,36 @@ const router = express.Router();
 const jwt = require('jsonwebtoken');
 const bcryptjs = require('bcryptjs');
 const { check,validationResult } = require('express-validator');
+const UserSchema = require('../schemas/User');
+const User = require("../schemas/User");
+
 
 router.post(
     '/signup',
     [
         check('email','E-mail is required').isEmail(),
-        check('password','Password is required').not.isEmpty()
+        check('password','Password is required').not().isEmpty(),
     ],
     async (req,res) => {
         try{
             const { email,password } = req.body;
-            const error = validationResult(req);
-            if(errors.isEmpty())
+            let user = await UserSchema.findOne({email : email});
+            const errors = validationResult(req);
+            if(!errors.isEmpty())
             {
-                
+                return res.status(401).json({errors : errors.array()});
             }
+
+            if(user){
+                return res.status(401).json({ msg : "There "})
+            }
+
+            user = new UserSchema({
+                email,
+                password
+             });
+
+            res.send('its is send');
         } catch (error){
             console.log(error.message);
             return res.status(500).json({ msg : "Server Error....."});
