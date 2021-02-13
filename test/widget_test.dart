2f88f00c65c0c08d2328be9d5dc7a84820bf8de0
vikash singh <46121493+vik4114@users.noreply.github.com>
2021-02-13 20:54:21 +0530

    Revert "Merge branch 'main' of https://github.com/yasharth291/notenexus into main"
    This reverts commit 682a02013649c4f1aa4a4c0f0db713d218f829fd, reversing
    changes made to 4f93eec1f9fa63a82e88d7f118341d2abc5ff656.

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -6,7 +6,6 @@
 // tree, read text, and verify that the values of widget properties are correct.
 
 import 'package:flutter/material.dart';
-import 'package:flutter/homepage.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 import 'package:notenexus/main.dart';
@@ -14,7 +13,7 @@ import 'package:notenexus/main.dart';
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(Home());
+    await tester.pumpWidget(MyApp());
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
